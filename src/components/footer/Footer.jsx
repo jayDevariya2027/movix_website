c53651a72d50f37a088861fc2cf104331517b2e2
jaydevariya2027 <jaydevariya2027@gmail.com>
2023-07-22 20:16:44 +0530

    movix code

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Disney+ Hotstar is an online video streaming platform owned by Novi
+          Digital Entertainment Private Limited, a wholly owned subsidiary of
+          Star India Private Limited. Disney+ Hotstar currently offers over
+          100,000 hours of TV content and movies across 9 languages, and every
+          major sport covered live. 
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
